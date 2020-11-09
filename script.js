8a6902033ef7c0742f224cd31f64404a96fd0def
archanaknayanar <archana@bigbinary.com>
2020-11-09 21:37:56 +0530

    made changes in JS file

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const colors = document.querySelector('.color');
-const sizes = document.querySelector('.size');
+
+const colors = document.querySelectorAll('.color');
+const image = document.querySelector('.image');
+
+colors.forEach(function (color){
+    color.addEventListener("click" , function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('grey')){
+            document.getElementById("#pic").src = "https://images-na.ssl-images-amazon.com/images/I/7100boX9WUL._SY535._SX._UX._SY._UY_.jpg";
+        }
+       
+    });
+    
+});
